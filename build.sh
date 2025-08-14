15b39c2897d4e54745f7ed573baa4019ce78db69
l-cuong@ruby-dev.vn <l-cuong@ruby-dev.vn>
2025-08-14 11:06:34 +0700

    feat: Add configuration files for Render deployment

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Download the Sentence Transformer model
+python -c "from sentence_transformers import SentenceTransformer; SentenceTransformer('all-MiniLM-L6-v2')"
